2e696c3aebce432e1fcc9a5b8c3169e01e4fb0ac
Pedro Valente Toledo <pedrovalentetoledo123@gmail.com>
2024-05-06 23:59:20 -0300

    Classes e ataque / receber dano

---
+++ Pokemon/Pokemon.h
@@ -0,0 +1,13 @@
+﻿// Pokemon.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
